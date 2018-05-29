9bd9b81f3062dc48e7493db774187ad8286a6e1e
tkctaiga <39551658+tkctaiga@users.noreply.github.com>
2018-05-29 16:53:15 +0900

    更新といらないファイル削除

--- src/b/dao/DeleteBookDao.java
+++ src/b/dao/DeleteBookDao.java
@@ -57,69 +57,7 @@ public class DeleteBookDao {
 		}
 	}
 
-	public List<bookBean> findsearch(String searchname, String category) throws DAOException {
-		if (con == null)
-			getConnection();
-		PreparedStatement st = null;
-		ResultSet rs = null;
-		try {
-			String sql;
-			if (searchname.equals("")) {
-				// ケース1 検索名なしカテゴリ検索なしの場合
-				if (category.equals("10")) {
-					sql = "SELECT DISTINCT b.bookinfo_isbn,c.category_name,b.bookinfo_name,b.bookinfo_author,b.publisher_name,bs.bookstate_rem FROM bookinfo b,category c,bookstate bs WHERE b.category_code = c.category_code AND b.bookinfo_isbn = bs.bookinfo_isbn AND bookstate_rem IS NULL";
-					st = con.prepareStatement(sql);
-
-				}
-				// ケース2 検索名なしカテゴリ検索ありの場合
-				else {
-					sql = "SELECT b.bookinfo_isbn,c.category_name,b.bookinfo_name,b.bookinfo_author,b.publisher_name FROM bookinfo b,category c WHERE b.category_code = ? AND b.category_code = c.category_code AND bookstate_rem IS NULL";
-					st = con.prepareStatement(sql);
-					st.setString(1, category);
-				}
-			} else {
-				// ケース3 検索名ありカテゴリ検索なしの場合
-				if (category.equals("10")) {
-					sql = "SELECT b.bookinfo_isbn,c.category_name,b.bookinfo_name,b.bookinfo_author,b.publisher_name FROM bookinfo b,category c WHERE b.category_code = c.category_code AND bookstate_rem IS NULL AND b.bookinfo_name LIKE ?";
-					st = con.prepareStatement(sql);
-					st.setString(1, "%" + searchname + "%");
-				}
-				// ケース4 検索名ありカテゴリ検索ありの場合
-				else {
-					sql = "SELECT b.bookinfo_isbn,c.category_name,b.bookinfo_name,b.bookinfo_author,b.publisher_name FROM bookinfo b,category c WHERE category_code = ? AND b.category_code = c.category_code AND bookstate_rem IS NULL AND b.bookinfo_name LIKE ?";
-					st = con.prepareStatement(sql);
-					st.setString(1, category);
-					st.setString(2, "%" + searchname + "%");
-				}
-			}
-			rs = st.executeQuery();
-			List<bookBean> list = new ArrayList<bookBean>();
-			while (rs.next()) {
-				String isbn = rs.getString("bookinfo_isbn");
-				String ccode = rs.getString("category_name");
-				String pcode = rs.getString("publisher_name");
-				String name = rs.getString("bookinfo_name");
-				String author = rs.getString("bookinfo_author");
-				bookBean bean = new bookBean(isbn, ccode, pcode, name, author);
-				list.add(bean);
-			}
-			return list;
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new DAOException("レコードの取得に失敗しました");
-		} finally {
-			try {
-				if (rs != null)
-					rs.close();
-				if (st != null)
-					st.close();
-				close();
-			} catch (Exception e) {
-				throw new DAOException("リソースの開放に失敗しました");
-			}
-		}
-	}
-
+	
 	public List<bookBean> findconfim(String uname) throws DAOException {
 		if (con == null)
 			getConnection();
